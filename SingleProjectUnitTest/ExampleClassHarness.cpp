4d5a0dbc1774dd484b3bc4e0d8e9fbe058fdaf0a
allen <allen.hunter@gmail.com>
2019-01-15 06:57:06 -0800

    moved tests to the implementation file of their harness after deciding that I prefer this architecture

--- SingleProjectUnitTest/ExampleClassHarness.cpp
+++ SingleProjectUnitTest/ExampleClassHarness.cpp
@@ -43,3 +43,28 @@ void ExampleClassHarness::SetUp(void)
 void ExampleClassHarness::TearDown(void)
 {
 }
+
+//###############################################################################################
+// Tests:
+//###############################################################################################
+TEST_F(ExampleClassHarness, isPositive)
+{
+	int v = 1;
+	bool res = false;
+	bool res_expected = true;
+
+	res = m_pExample->isPositive(v);
+
+	ASSERT_EQ(res, res_expected);
+}
+
+TEST_F(ExampleClassHarness, isNegative)
+{
+	int v = -1;
+	bool res = false;
+	bool res_expected = true;
+
+	res = m_pExample->isNegative(v);
+
+	ASSERT_EQ(res, res_expected);
+}
--- SingleProjectUnitTest/UnitTests.cpp
+++ SingleProjectUnitTest/UnitTests.cpp
@@ -1,29 +1,9 @@
 #include "pch.h"
 #include "ExampleClassHarness.h"
 
-TEST_F(ExampleClassHarness, isPositive)
-{
-	int v = 1;
-	bool res = false;
-	bool res_expected = true;
 
-	res = m_pExample->isPositive(v);
-
-	ASSERT_EQ(res, res_expected);
-}
-
-TEST_F(ExampleClassHarness, isNegative)
-{
-	int v = -1;
-	bool res = false;
-	bool res_expected = true;
-
-	res = m_pExample->isNegative(v);
-
-	ASSERT_EQ(res, res_expected);
-}
 
 TEST(GenericExamples, StreamAppend)
 {
-	EXPECT_TRUE(false) << "demonstrating the appending of a stream";
+	EXPECT_EQ(true,false) << "demonstrating the appending of a stream.  This is supposed to fail.";
 }
